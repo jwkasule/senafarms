0c8ef645954bee33813801473dd5c970ea869320
jwkasule <60119004+jwkasule@users.noreply.github.com>
2026-09-01 09:59:20 +0100

    Call AdminAndSampleSeeder from DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -6,6 +6,6 @@ class DatabaseSeeder extends Seeder
 {
     public function run()
     {
-        $this->call([UsersTableSeeder::class, CustomersTableSeeder::class, InventoriesTableSeeder::class]);
+        $this->call([UsersTableSeeder::class, CustomersTableSeeder::class, InventoriesTableSeeder::class, AdminAndSampleSeeder::class]);
     }
 }
